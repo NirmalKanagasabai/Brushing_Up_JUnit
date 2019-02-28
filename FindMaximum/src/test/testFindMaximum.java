13a2b3adf2491ff7fd28ceb4bc0bc23e0b215ca7
Nirmal Kanagasabai <nirmal.kanagasabai@gmail.com>
2019-02-27 23:47:01 -0500

    Example 1: Find Maximum

--- FindMaximum/src/test/testFindMaximum.java
+++ FindMaximum/src/test/testFindMaximum.java
@@ -8,9 +8,9 @@ public class testFindMaximum {
 	
 	@Test
 	public void testFindMax() {
-		assertEquals(4, FindMaximum.findMaximum(new int[] {3, 1, 4, 0, -5}));
-		assertEquals(-1, FindMaximum.findMaximum(new int[] {-9, -5, -3, -1}));
-		assertTrue(5 > FindMaximum.findMaximum(new int[] {3, 1, 4})); 
+		assertEquals(4, FindMaximum.findMaximum(new int[] {3, 1, 4, 0, -5})); // Expected vs. Actual
+		assertEquals(-1, FindMaximum.findMaximum(new int[] {-9, -5, -3, -1})); // Expected vs. Actual
+		assertTrue(5 > FindMaximum.findMaximum(new int[] {3, 1, 4})); // Condition
 	}
 
 }
